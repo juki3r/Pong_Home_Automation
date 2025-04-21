d5e4f80b62983326e208828ed0dc8e34fe4b3e20
juki3r <deepong25@gmail.com>
2025-04-21 13:21:42 +0800

    modified:   app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
 
 class User extends Authenticatable
 {
@@ -46,5 +48,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($user) {
+            $user->api_key = Str::uuid(); // or use Str::random(32);
+        });
     }
 }
