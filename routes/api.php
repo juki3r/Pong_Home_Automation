bd52865aae58e60e204361bfc92289d15f667e73
juki3r <deepong25@gmail.com>
2025-04-21 13:31:29 +0800

    modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-api', function(){
-    return response()->json(['message' => 'Hello API!']);
-});
+// Route::get('/test-api', function(){
+//     return response()->json(['message' => 'Hello API!']);
+// });
 
 Route::middleware('check.api')->get('/test-api', function () {
     return response()->json(['message' => 'Welcome! Your API key is valid and you’re subscribed.']);
