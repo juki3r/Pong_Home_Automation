ae48efa417833300f6c71f79d5776ebffac8ebee
juki3r <deepong25@gmail.com>
2025-04-21 23:26:19 +0800

    modified:   app/Http/Controllers/MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -34,33 +34,39 @@ class MainController extends Controller
             $user->save();
         }
 
-        // Avoid duplicating lights
+       // Avoid duplicating lights
         if ($user->lights()->count() === 0) {
+            $gpioPins = [21, 22, 19, 18, 5, 4, 2];  // GPIOs for ESP32
             $defaultLights = [];
-            for ($i = 1; $i <= 7; $i++) {
+
+            foreach ($gpioPins as $index => $gpio) {
                 $defaultLights[] = [
-                    'gpio' => 'D' . $i,
-                    'switch_name' => 'switch-' . $i,
+                    'gpio' => (string) $gpio,
+                    'switch_name' => 'switch-' . ($index + 1),
                     'action' => 'off',
                     'status' => 'pending',
                     'switch_status' => 'off',
                 ];
             }
+
             $user->lights()->createMany($defaultLights);
         }
 
-        // Avoid duplicating appliances
+
         if ($user->appliances()->count() === 0) {
+            $gpioPins = [21, 22, 19, 18, 5, 4, 2];  // GPIOs for ESP32
             $defaultAppliances = [];
-            for ($i = 1; $i <= 7; $i++) {
+
+            foreach ($gpioPins as $index => $gpio) {
                 $defaultAppliances[] = [
-                    'gpio' => 'D' . $i,
-                    'switch_name' => 'appliance-' . $i,
+                    'gpio' => (string) $gpio,
+                    'switch_name' => 'switch-' . ($index + 1),
                     'action' => 'off',
                     'status' => 'pending',
                     'switch_status' => 'off',
                 ];
             }
+
             $user->appliances()->createMany($defaultAppliances);
         }
 
