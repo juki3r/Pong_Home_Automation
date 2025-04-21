a48f9b94b8765050b2ed5454417ce1b7ae5f2a36
juki3r <deepong25@gmail.com>
2025-04-21 14:11:49 +0800

    new file:   app/Http/Controllers/MainController.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Main Controller
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [MainController::class, 'shwo_dashboard'])->name('dashboard');
+});
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
