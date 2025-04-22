d6e0a67ea8e603a3b8e04c469dc94a292f9d0bcd
juki3r <deepong25@gmail.com>
2025-04-22 22:02:01 +0800

    modified:   resources/views/lights/index.blade.php

--- resources/views/lights/index.blade.php
+++ resources/views/lights/index.blade.php
@@ -11,32 +11,30 @@
                 <div class="p-6 text-gray-900">
                     {{ __("Lights") }}
 
-                    <!-- Modal Trigger -->
-                    <button @click="open = true"
-                        class="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                    <!-- Trigger Button -->
+                    <button type="button" class="btn btn-primary mt-4" data-bs-toggle="modal" data-bs-target="#lightModal">
                         Open Modal
                     </button>
-                    <button>Demo</button>
 
                     <!-- Modal -->
-                    <div x-data="{ open: false }">
-                        <div x-show="open" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
-                            <div class="bg-white w-full max-w-md p-6 rounded-lg shadow-lg relative">
-                                <h3 class="text-lg font-semibold mb-4">Light Settings</h3>
-                                <p class="text-gray-700 mb-4">Customize your lighting preferences here.</p>
-                                <div class="flex justify-end space-x-2">
-                                    <button @click="open = false"
-                                        class="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">
-                                        Cancel
-                                    </button>
-                                    <button @click="open = false"
-                                        class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                                        Save
-                                    </button>
+                    <div class="modal fade" id="lightModal" tabindex="-1" aria-labelledby="lightModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="lightModalLabel">Light Settings</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Customize your lighting preferences here.
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- End Modal -->
 
                 </div>
             </div>
